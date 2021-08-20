27f04ddd3ac0d196a1613654e5bb927963135c6f
Ignacio <vazquez_ignacio_martin@hotmail.com>
2021-08-19 22:59:50 -0300

    asd

--- indexjs.js
+++ indexjs.js
@@ -1,38 +1,50 @@
-$(function() {
-	$('.toggler').on('click', function() {
+$(function () {
+	$('.toggler').on('click', function () {
 		$('nav').slideToggle(500);
-  });
+	});
 });
 
+// SELECTORES DE FORMAS DE PAGO
+const debitSelector = document.getElementById('debitSelector');
+const cashSelector = document.getElementById('cashSelector');
+const depositSelector = document.getElementById('depositSelector');
 
-const debitSelector = document.getElementById("debitSelector");
-const cashSelector = document.getElementById("cashSelector");
-const depositSelector = document.getElementById("depositSelector");
-const debitForm = document.getElementById("debitForm");
-const transferMessage = document.getElementById("transferMessage");
+const debitForm = document.querySelectorAll('[id=debitForm]');
+const transferMessage = document.getElementById('transferMessage');
 
-debitSelector.addEventListener("mouseover","click" () => {
-	debitSelector.src = "SVG-Mobile/debit-selected.svg ";
-	
-  });
-  cashSelector.addEventListener("mouseover", () => {
-	cashSelector.src = "SVG-Mobile/cash-pay-selected.svg";
-	
-  });
-  depositSelector.addEventListener("mouseover", () => {
-	depositSelector.src = "SVG-Mobile/deposit-transf-selected.svg";
-	
-  });
-  
-  debitSelector.addEventListener("mouseleave", () => {
-	debitSelector.src = "SVG-Mobile/credit-debit.svg";
-	
-  });
-  cashSelector.addEventListener("mouseleave", () => {
-	cashSelector.src = "SVG-Mobile/cash-pay.svg";
-	
-  });
-  depositSelector.addEventListener("mouseleave", () => {
-	depositSelector.src = "SVG-Mobile/deposit-transf.svg";
-	
-  });
+//ON HOVER FORMAS DE PAGO
+debitSelector.addEventListener('mouseover', () => {
+	debitSelector.src = 'SVG-Mobile/debit-selected.svg ';
+});
+cashSelector.addEventListener('mouseover', () => {
+	cashSelector.src = 'SVG-Mobile/cash-pay-selected.svg';
+});
+depositSelector.addEventListener('mouseover', () => {
+	depositSelector.src = 'SVG-Mobile/deposit-transf-selected.svg';
+});
+
+debitSelector.addEventListener('mouseleave', () => {
+	debitSelector.src = 'SVG-Mobile/credit-debit.svg';
+});
+cashSelector.addEventListener('mouseleave', () => {
+	cashSelector.src = 'SVG-Mobile/cash-pay.svg';
+});
+depositSelector.addEventListener('mouseleave', () => {
+	depositSelector.src = 'SVG-Mobile/deposit-transf.svg';
+});
+//
+
+//ON CLICK FORMAS DE PAGO
+debitSelector.addEventListener('click', () => {
+ 
+	transferMessage.style.display = 'none';
+  debitForm.forEach(form => (form.style.display = 'block'));
+
+});
+
+depositSelector.addEventListener('click', () => {
+	debitForm.forEach(form => (form.style.display = 'none'));
+	transferMessage.style.display = 'block';
+});
+
+//
