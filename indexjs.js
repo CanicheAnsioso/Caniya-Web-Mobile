c11007241fd575e40e4b3bfe0937865812fb9a79
CanicheAnsioso <layla.elizabeth95@gmail.com>
2021-08-23 19:24:13 -0300

    cositas

--- indexjs.js
+++ indexjs.js
@@ -6,7 +6,6 @@ $(function () {
 
 // SELECTORES DE FORMAS DE PAGO
 const debitSelector = document.getElementById('debitSelector');
-const cashSelector = document.getElementById('cashSelector');
 const depositSelector = document.getElementById('depositSelector');
 
 const debitForm = document.querySelectorAll('[id=debitForm]');
@@ -16,9 +15,7 @@ const transferMessage = document.getElementById('transferMessage');
 debitSelector.addEventListener('mouseover', () => {
 	debitSelector.src = 'SVG-Mobile/debit-selected.svg ';
 });
-cashSelector.addEventListener('mouseover', () => {
-	cashSelector.src = 'SVG-Mobile/cash-pay-selected.svg';
-});
+
 depositSelector.addEventListener('mouseover', () => {
 	depositSelector.src = 'SVG-Mobile/deposit-transf-selected.svg';
 });
@@ -26,9 +23,7 @@ depositSelector.addEventListener('mouseover', () => {
 debitSelector.addEventListener('mouseleave', () => {
 	debitSelector.src = 'SVG-Mobile/credit-debit.svg';
 });
-cashSelector.addEventListener('mouseleave', () => {
-	cashSelector.src = 'SVG-Mobile/cash-pay.svg';
-});
+
 depositSelector.addEventListener('mouseleave', () => {
 	depositSelector.src = 'SVG-Mobile/deposit-transf.svg';
 });
